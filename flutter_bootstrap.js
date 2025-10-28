d071c12c2ae89385d0bc6c84757fe323ce455ac5
Axel Nava Hernandez <axel@tuhogarconsentido.com>
2025-10-28 14:09:37 -0600

    Feat

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3718046789"
+    serviceWorkerVersion: "1463279520"
   }
 });
